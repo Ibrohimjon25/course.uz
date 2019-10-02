6ca3d474645dfeee4e7a87a633ee30309477caff
Ibrohimjon <tuitcoder@gmail.com>
2019-10-02 21:27:53 +0500

    verstka yuklandi

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,11 +21,26 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        "css/style.css",
+        "css/bootstrap.min.css",
+        "css/owl.carousel.min.css",
+        "magnific-popup.css",
+        "css/font-awesome.min.css",
+        "css/custom-icon.css",
+        "css/classy-nav.min.css",
+        "css/animate.css"
+        
     ];
     public $js = [
+        "js/jquery/jquery-2.2.4.min.js",
+        "js/bootstrap/popper.min.js",
+        "js/bootstrap/bootstrap.min.js",
+        "js/plugins/plugins.js",
+        "js/active.js",
+        "js/extention/choices.js",
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        // 'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
